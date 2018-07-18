3cfb72808cdb6d74bedb0de7a7b8ce72c9e2c359
Grisell Reyes Rios <37979454+grisreyesrios@users.noreply.github.com>
2018-07-18 14:26:27 +0200

    Create style.css

---
+++ Inspirational_quotes_JavaScript/style.css
@@ -0,0 +1,51 @@
+/* The device with borders */
+.smartphone {
+  position: relative;
+  width: 360px;
+  height: 640px;
+  margin: auto;
+  border: 25px black solid;
+  border-top-width: 70px;
+  border-bottom-width: 60px;
+  border-radius: 40px;
+}
+
+/* The circle on the bottom of the device */
+.smartphone:after {
+  content: '';
+  display: block;
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  left: 50%;
+  bottom: -65px;
+  transform: translate(-50%, -50%);
+  background: #333;
+  border-radius: 40%;
+}
+
+/* The screen (or content) of the device */
+.smartphone .content {
+  width: 360px;
+  height: 640px;
+  background: white;
+}
+
+.header {
+  padding: 10px 16px;
+  background: #555;
+  color: #f1f1f1;
+  text-align: center;
+}
+paragraph{
+  text-align: justify;
+}
+.tab { 
+       display:inline-block; 
+       margin-left: 20px; 
+}
+#more {display: none;}
+#more2 {display: none;}
+#more3 {display: none;}
+#more4 {display: none;}
+
